daacf2fed6dc5b5ad4723b5f81758c7c5915e00f
LS8 <lennartschoettker@hotmail.com>
2017-03-19 15:48:13 +0100

    initialization

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+var app = () => console.log(`This super cool app was started at ${Date.now().toLocaleString()}!`);
+
+var myApp = app(); 
+// let i = 4;
+// const arr = [];
+// var test = 342;
+
+// (function log() {
+//   return 'do something different my friend';
+// }());
+
+// i += 2;
+// arr[0] = i;
+
+// function square() {
+//   return arr[0] * arr[0];
+// }
+
+// square();
