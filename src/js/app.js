4d9257c7f2d963b38305a6e016e04c560c76091d
LS8 <lennartschoettker@hotmail.com>
2017-03-21 11:59:40 +0100

    shorten url if a long url is passed and insert into the db or return it if it is already in the db

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,45 @@
 const express = require('express');
 
-// const mongo = require('mongodb').MongoClient;
+const config = require('./config.js');
+
+const dbAdress = config.db.host + ':27017/' + config.db.name;
+
+const db = require('monk')(dbAdress);
 
 const urlHelper = require('./url.js');
 
-const legitUrl = urlHelper.legitUrl;
-
-const app = express();
-
-// const config = require('./config');
 const hash = require('./hash.js');
+
+const legitUrl = urlHelper.legitUrl;
+const urls = db.get('urls');
+const app = express();
+// encodes a number(id) to base-36, which can be used as short url
+const encode = hash.encode;
+const decode = hash.decode;
+let counter = 0;
+
 
 app.get('*', (req, res) => {
   const url = req.params['0'].slice(1);
-  const validUrl = legitUrl(url);
-  if (!validUrl) {
-    res.send('invalid url');
+  const validUrlToShorten = legitUrl(url);
+  if (validUrlToShorten) {
+    urls.findOne({ longUrl: url }).then((doc) => {
+      // if url is already in the db return it
+      if (doc) {
+        res.send(doc);
+      } else {
+        // shorten the url and add it to the db
+        counter += 1;
+        const urlObj = {
+          longUrl: url,
+          shortUrl: encode(counter),
+        };
+        urls.insert(urlObj);
+      }
+      res.end();
+    });
+  } else {
+
   }
 });
 
