729803a1507598c3756220c1849363691a7de31a
LS8 <lennartschoettker@hotmail.com>
2017-03-21 17:22:43 +0100

    update

--- app/config.js
+++ app/config.js
@@ -1,10 +1,10 @@
 const config = {};
 
 config.db = {};
-config.webhost = 'localhost:8080/';
+config.webhost = 'https://url-shortener-ls8.herokuapp.com/';
 
 // your MongoDB host and database name
-config.db.host = 'localhost';
+// config.db.host = 'localhost';
 config.db.name = 'url_shortener';
 
 module.exports = config;
